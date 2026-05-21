2491964ecd3eaf9a7473155d8839ff1db11a540e
Julian_Danilo_Brito_Duran <juliandanilobd@msmk.university>
2026-05-22 01:38:03 +0200

    fix: typo error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import pytest
 
-from app.domain.match.match import Match
-
-
 @pytest.fixture
 def sample_match():
-
+    from app.domain.match.match import Match
     match = Match("PVP")
     match.initialize()
 
     return match
+
+
+
+
