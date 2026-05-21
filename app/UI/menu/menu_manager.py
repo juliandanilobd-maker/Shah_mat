5337fd35dc78c88290c5dd0c16a3ab82821736d5
Julian_Danilo_Brito_Duran <juliandanilobd@msmk.university>
2026-05-22 01:37:26 +0200

    fix: typo error do not meet quality criteria

--- app/UI/menu/menu_manager.py
+++ app/UI/menu/menu_manager.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-class MenuManager():
-
-    def show_main_menu(self):
-        return '0'
-=======
 class MenuManager:
 
     def show_main_menu(self):
         return "0"
->>>>>>> develop
+
